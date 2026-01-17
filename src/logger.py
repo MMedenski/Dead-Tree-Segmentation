b84eaa1d84e4fbfcd800c6fe73124845068f41e1
MMedenski <medenskimarcin@gmail.com>
2026-01-17 20:38:35 +0100

    Add files via upload

---
+++ src/logger.py
@@ -0,0 +1,29 @@
+import logging
+import sys
+
+def setup_logger(
+    name: str = "dead_tree",
+    level: int = logging.INFO,
+    log_file: str | None = None
+) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if log_file:
+        file_handler = logging.FileHandler(log_file, encoding="utf-8")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
